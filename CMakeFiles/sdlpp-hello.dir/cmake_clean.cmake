24f355e39ea74cda5ddf82dc8e92e8b5e7923263
clezious <manuelreberendo@gmail.com>
2021-11-25 01:04:43 -0300

    reestructuracion estructura de archivos

---
+++ CMakeFiles/sdlpp-hello.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sdlpp-hello.dir/src/client/board.cpp.o"
+  "CMakeFiles/sdlpp-hello.dir/src/client/mainSdlHello.cpp.o"
+  "CMakeFiles/sdlpp-hello.dir/src/client/menu.cpp.o"
+  "CMakeFiles/sdlpp-hello.dir/src/client/piece.cpp.o"
+  "CMakeFiles/sdlpp-hello.dir/src/client/spot.cpp.o"
+  "sdlpp-hello"
+  "sdlpp-hello.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sdlpp-hello.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeFiles/server.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/manu/Desktop/tallerTpGtupal/src/server/bishop.cpp" "/home/manu/Desktop/tallerTpGtupal/CMakeFiles/server.dir/src/server/bishop.cpp.o"
+  "/home/manu/Desktop/tallerTpGtupal/src/server/board.cpp" "/home/manu/Desktop/tallerTpGtupal/CMakeFiles/server.dir/src/server/board.cpp.o"
+  "/home/manu/Desktop/tallerTpGtupal/src/server/king.cpp" "/home/manu/Desktop/tallerTpGtupal/CMakeFiles/server.dir/src/server/king.cpp.o"
+  "/home/manu/Desktop/tallerTpGtupal/src/server/knight.cpp" "/home/manu/Desktop/tallerTpGtupal/CMakeFiles/server.dir/src/server/knight.cpp.o"
+  "/home/manu/Desktop/tallerTpGtupal/src/server/main.cpp" "/home/manu/Desktop/tallerTpGtupal/CMakeFiles/server.dir/src/server/main.cpp.o"
+  "/home/manu/Desktop/tallerTpGtupal/src/server/pawn.cpp" "/home/manu/Desktop/tallerTpGtupal/CMakeFiles/server.dir/src/server/pawn.cpp.o"
+  "/home/manu/Desktop/tallerTpGtupal/src/server/piece.cpp" "/home/manu/Desktop/tallerTpGtupal/CMakeFiles/server.dir/src/server/piece.cpp.o"
+  "/home/manu/Desktop/tallerTpGtupal/src/server/queen.cpp" "/home/manu/Desktop/tallerTpGtupal/CMakeFiles/server.dir/src/server/queen.cpp.o"
+  "/home/manu/Desktop/tallerTpGtupal/src/server/rook.cpp" "/home/manu/Desktop/tallerTpGtupal/CMakeFiles/server.dir/src/server/rook.cpp.o"
+  "/home/manu/Desktop/tallerTpGtupal/src/server/square.cpp" "/home/manu/Desktop/tallerTpGtupal/CMakeFiles/server.dir/src/server/square.cpp.o"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "libs"
+  "libs/libSDL2pp"
+  "/usr/include/SDL2"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
