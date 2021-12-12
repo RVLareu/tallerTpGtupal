103a2c80c1eb348bca0d2ad0b124426d312fa62f
clezious <manuelreberendo@gmail.com>
2021-12-12 19:52:07 -0300

    fix instancias split protocol

--- src/common/common_protocol.cpp
+++ src/common/common_protocol.cpp
@@ -61,20 +61,17 @@ void Protocol::send_board_status(Socket& socket,
         // Se resaltan sus instancias
         vector<tuple<int, int>> piece_instances_positions = board.get_piece_instances_positions(std::get<0>(selected_pos), std::get<1>(selected_pos));
         for (const auto& position: piece_instances_positions) {
-            if (std::get<0>(position) != std::get<0>(selected_pos) and std::get<1>(position) != std::get<1>(selected_pos)) { // no se resalta a la pieza seleccionada
+            if (position != selected_pos){// no se resalta a la pieza seleccionada
                 vector_board.push_back('h'); //(h)ighlight
                 vector_board.push_back(std::get<0>(position));
                 vector_board.push_back(std::get<1>(position));
                 vector_board.push_back('i'); // (i)nstancia de la misma pieza seleccionada
+                std::cout << "INSTANCIA: " << std::get<0>(position) << " " << std::get<1>(position) << std::endl;
             }
         }
         vector<tuple<int, int>> selected_piece_possible_movements = 
             board.get_piece_possible_movements(std::get<0>(selected_pos),std::get<1>(selected_pos));       
         for (const auto& position: selected_piece_possible_movements) {
-            // vector_board.push_back('h'); //(h)ighlight
-            // vector_board.push_back(std::get<0>(selected_pos));
-            // vector_board.push_back(std::get<1>(selected_pos));
-            // vector_board.push_back('s');// Pieza (s)eleccionada
             vector<tuple<int, int>> selected_piece_possible_movements = 
                 board.get_piece_possible_movements(std::get<0>(selected_pos),std::get<1>(selected_pos));       
             for (const auto& position: selected_piece_possible_movements) {
